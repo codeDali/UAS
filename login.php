1aaa66bed31e3ae193bbdd14e757046096e840d2
codeDali <rizkyalpariji@students.usu.ac.id>
2023-11-27 14:34:09 +0700

    update

--- login.php
+++ login.php
@@ -1,3 +1,25 @@
 <?php
 
-include
+include 'connection.php';
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    session_start();
+    
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $query = "SELECT * FROM user WHERE username='$username' AND password='$password' ";
+    
+    $result = $conn->query($query);
+    $isExist = $result->num_rows;
+
+
+    if($isExist){
+        $_SESSION['username'] =$username;
+        header("location:home.php");
+    } else{
+        echo '<script>alert("Username atau password salah")</script>';
+    }
+}
+
+
+?>
